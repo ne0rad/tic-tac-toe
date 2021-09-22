a613fb99ed78a2e1e089b487c021dc0a20e2f373
ne0rad <usackasr@gmail.com>
2021-09-22 17:48:12 +0100

    play/reset functions for gameboard module

--- js/modules/gameboard.js
+++ js/modules/gameboard.js
@@ -1,16 +1,19 @@
 // Module for manipulating the game board
 
+        // Board coordinates (x,y):
+        // | 1,1 | 1,2 | 1,3 |
+        //  -----------------
+        // | 2,1 | 2,2 | 2,3 |
+        //  -----------------
+        // | 3,1 | 3,2 | 3,3 |
+
 const gameboard = (() => {
-    const boardArr = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+    const boardArr = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 
     const square = (x, y) => {
-    // return value of the square based on coordinates
-    // | 1,1 | 1,2 | 1,3 |
-    //  -----------------
-    // | 2,1 | 2,2 | 2,3 |
-    //  -----------------
-    // | 3,1 | 3,2 | 3,3 |
-        if(x > 3 || y > 3) return -1;
+        // return value of the square based on coordinates
+
+        if(x > 3 || y > 3) return NaN;
         switch (x) {
             case 1:
                 return boardArr[y-1];
@@ -22,7 +25,39 @@ const gameboard = (() => {
                 break;
         }
     }
-    return {square};
+
+    const play = (sign, x, y) => {
+        // sign is "x" or "o"
+        // put X or O on the board based on coordinates
+        if(square(x,y) !== -1) return false;
+        if(sign != "x" || sign != "o") return false;
+
+        if(sign == "x") sign = 0;
+        else sign = 1;
+
+        switch (x) {
+            case 1:
+                boardArr[y-1] = sign;
+                break;
+            case 2:
+                boardArr[y+2] = sign;
+                break;
+            case 3:
+                boardArr[y+5] = sign;
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    const reset = () => {
+        for (let i = 0; i < boardArr.length; i++) {
+            boardArr[i] = -1; 
+        }
+    }
+
+    return {square, play, reset};
 })();
 
 export default gameboard;
